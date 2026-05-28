e0fbb79b9653a8a29c14c97d6cd4f78fa6b64aa1
Sahan Harshajith <sahanharshajith02@gmail.com>
2026-05-28 16:21:10 +0530

    Add gold views for customers, products, sales

---
+++ scripts/gold/SQLQuery1.sql
@@ -0,0 +1,70 @@
+USE DataWarehouse;
+
+SELECT cst_id, COUNT(*) 
+FROM (
+	SELECT 
+		ci.cst_id,
+		ci.cst_key,
+		ci.cst_firstname,
+		ci.cst_lastname,
+		ci.cst_marital_status,
+		ci.cst_gndr,
+		ci.cst_create_date,
+		ca.bdate,
+		ca.gen,
+		la.cntry
+	FROM silver.crm_cust_info ci
+	LEFT JOIN silver.erp_cust_az12 ca ON ci.cst_key = ca.cid
+	LEFT JOIN silver.erp_loc_a101 la ON ci.cst_key = la.cid
+)t GROUP BY cst_id
+HAVING COUNT(*) > 1;
+
+SELECT DISTINCT
+	ci.cst_gndr,
+	ca.gen,
+	CASE
+		WHEN ci.cst_gndr != 'n/a' THEN ci.cst_gndr -- CRM is the Master for gender Info
+		ELSE COALESCE(ca.gen, 'n/a') 
+	END new_gen
+FROM silver.crm_cust_info ci
+LEFT JOIN silver.erp_cust_az12 ca ON ci.cst_key = ca.cid
+LEFT JOIN silver.erp_loc_a101 la ON ci.cst_key = la.cid
+ORDER BY 1, 2;
+
+SELECT 
+	ca.cid,
+	ca.bdate,
+	ca.gen
+FROM silver.erp_cust_az12 ca; 
+
+SELECT 
+	la.cid,
+	la.cntry
+FROM silver.erp_loc_a101 la; 
+
+
+GO
+CREATE OR ALTER VIEW gold.dim_customers AS
+SELECT 
+	ROW_NUMBER() OVER (ORDER BY cst_id) AS customer_key,
+	ci.cst_id AS customer_id,
+	ci.cst_key AS customer_number,
+	ci.cst_firstname AS first_name,
+	ci.cst_lastname AS last_name,
+	la.cntry AS country,
+	ci.cst_marital_status AS marital_status,
+	CASE
+		WHEN ci.cst_gndr != 'n/a' THEN ci.cst_gndr -- CRM is the Master for gender Info
+		ELSE COALESCE(ca.gen, 'n/a') 
+	END AS gender,
+	ca.bdate AS birthdate,
+	ci.cst_create_date AS create_date
+FROM silver.crm_cust_info ci
+LEFT JOIN silver.erp_cust_az12 ca ON ci.cst_key = ca.cid
+LEFT JOIN silver.erp_loc_a101 la ON ci.cst_key = la.cid;
+
+SELECT *
+FROM gold.dim_customers;
+
+SELECT DISTINCT gender
+FROM gold.dim_customers;
---
+++ scripts/gold/SQLQuery2.sql
@@ -0,0 +1,58 @@
+USE DataWarehouse
+
+SELECT * FROM silver.crm_prd_info;
+
+SELECT * FROM silver.erp_px_cat_g1v2;
+
+SELECT 
+	pn.prd_id,
+	pn.cat_id,
+	pn.prd_key,
+	pn.prd_nm,
+	pn.prd_cost,
+	pn.prd_line,
+	pn.prd_start_dt,
+	pn.prd_end_dt
+FROM silver.crm_prd_info pn
+WHERE pn.prd_end_dt IS NULL;
+
+SELECT prd_key, COUNT(*)
+FROM (
+	SELECT 
+		pn.prd_id,
+		pn.cat_id,
+		pn.prd_key,
+		pn.prd_nm,
+		pn.prd_cost,
+		pn.prd_line,
+		pn.prd_start_dt,
+		pc.id,
+		pc.cat,
+		pc.subcat,
+		pc.maintenance
+	FROM silver.crm_prd_info pn
+	LEFT JOIN silver.erp_px_cat_g1v2 pc ON pn.cat_id = pc.id
+	WHERE pn.prd_end_dt IS NULL
+)t GROUP BY prd_key
+HAVING COUNT(*) > 1;
+
+
+GO
+CREATE OR ALTER VIEW gold.dim_products AS
+SELECT 
+	ROW_NUMBER() OVER(ORDER BY pn.prd_start_dt, pn.prd_key) AS product_key,
+	pn.prd_id AS product_id,
+	pn.prd_key AS product_number,
+	pn.prd_nm AS product_name,
+	pn.cat_id AS category_id,
+	pc.cat AS category,
+	pc.subcat AS subcategory,
+	pc.maintenance,
+	pn.prd_cost AS product_cost,
+	pn.prd_line AS product_line,
+	pn.prd_start_dt AS start_date
+FROM silver.crm_prd_info pn
+LEFT JOIN silver.erp_px_cat_g1v2 pc ON pn.cat_id = pc.id
+WHERE pn.prd_end_dt IS NULL -- Filter out all historical data
+
+SELECT * FROM gold.dim_products;
